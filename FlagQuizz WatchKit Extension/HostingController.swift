f8e8044c5b4be197d468458d264fef266a03dd81
daniel <daniel.d4@icloud.com>
2019-11-25 20:17:37 +0100

    Initial Commit

---
+++ FlagQuizz WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  FlagQuizz WatchKit Extension
+//
+//  Created by daniel d4 on 25/11/2019.
+//  Copyright © 2019 Remi Herbiet. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
